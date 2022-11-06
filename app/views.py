1f1c76f0da0410b493c09031699295781b875ec9
zsikorska <sikorska.zuzia@gmail.com>
2022-11-06 13:55:38 +0000

    basic template for project

---
+++ app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from datetime import datetime
+
+
+# Create your views here.
+
+def home(request):
+    tparams = {
+        'title': 'Home Page',
+        'year': datetime.now().year,
+    }
+    return render(request, 'index.html', tparams)
+
+
+def contact(request):
+    tparams = {
+        'title': 'Contact',
+        'message': 'Your contact page.',
+        'year': datetime.now().year,
+    }
+    return render(request, 'contact.html', tparams)
+
+
+def about(request):
+    tparams = {
+        'title': 'About',
+        'message': 'Your application description page.',
+        'year': datetime.now().year,
+    }
+    return render(request, 'about.html', tparams)
